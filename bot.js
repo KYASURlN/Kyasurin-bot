a3abe47417c3cbaf343cde2cae26ecf6207425b5
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2017-10-28 17:13:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,18 @@ client.on('message', message => {
     if (message.content === 'tomo is gay') {
     	message.reply('THIS IS VERY MUCH TRUE');
   	}
+    if (message.content === 'wheres kyo') {
+    	message.reply('im right fucking here');
+  	}
+    if (message.content === 'where is kyo') {
+    	message.reply('probably busy or dead');
+  	}
+    if (message.content === 'I LOVE KYO') {
+    	message.reply('I LOVE YOU TOO unless ur tomo');
+  	}
+    if (message.content === 'where is kyo') {
+    	message.reply('probably busy or dead');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY

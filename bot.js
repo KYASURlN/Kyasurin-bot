b72f72058a798c090d94ade3b53bd31226ee258a
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2017-10-29 00:10:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,8 +21,20 @@ client.on('message', message => {
     if (message.content === 'I LOVE KYO') {
     	message.reply('I LOVE YOU TOO unless ur tomo');
   	}
-    if (message.content === 'where is kyo') {
-    	message.reply('probably busy or dead');
+    if (message.content === 'i am gay for kyo') {
+    	message.reply('woah there buckaroo');
+  	}
+    if (message.content === 'i love kyo') {
+    	message.reply('i love you too?? i guess??');
+  	}
+    if (message.content === 'kyo i love you') {
+    	message.reply('//SCREAMS IN VIETNAMESE');
+  	}
+    if (message.content === 'tai sao toi lam may') {
+    	message.reply('tai vi may ngu ^ q ^');
+  	}
+    if (message.content === 'TOI YEU KYO') {
+    	message.reply('KYO YEU EM LUON!!');
   	}
 });
 

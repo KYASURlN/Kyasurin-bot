12c1212c64b25d65f7fa8ebed349ae31de390baf
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-02-13 18:06:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,73 +5,17 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong right back at ya');
-  	}
-    if (message.content === 'tomo is gay') {
-    	message.reply('THIS IS VERY MUCH TRUE');
-  	}
-    if (message.content === 'wheres kyo') {
-    	message.reply('im right fucking here');
-  	}
-    if (message.content === 'where is kyo') {
-    	message.reply('probably busy or dead');
-  	}
-    if (message.content === 'I LOVE KYO') {
-    	message.reply('I LOVE YOU TOO unless ur tomo');
-  	}
-    if (message.content === 'i am gay for kyo') {
-    	message.reply('woah there buckaroo');
-  	}
-    if (message.content === 'i love kyo') {
-    	message.reply('i love you too?? i guess??');
-  	}
-    if (message.content === 'kyo i love you') {
-    	message.reply('//SCREAMS IN VIETNAMESE');
-  	}
-    if (message.content === 'tai sao toi lam may') {
-    	message.reply('tai vi may ngu ^ q ^');
-  	}
-    if (message.content === 'TOI YEU KYO') {
-    	message.reply('KYO YEU EM LUON!!');
-  	}
-    if (message.content === 'im straight') {
-    	message.reply('well thats a lie');
-  	}
-    if (message.content === 'IM STRAIGHT') {
-    	message.reply('sure jan');
-  	}
-    if (message.content === 'IM LOYAL') {
-    	message.reply('sure jan,, sure');
-  	}
-    if (message.content === 'nana is gay') {
-    	message.reply('gay for me ^ q ^');
-  	}
-    if (message.content === 'WO AI NI') {
-    	message.reply('WO AI NI BACK AT YA');
-    }
-    if (message.content === 'IM NOT GAY') {
-    	message.reply('But thats Tomos lie, isnt it?');
-    }
-    if (message.content === 'im not gay') {
-    	message.reply('greatest joke of 2k17');
-    }
-    if (message.content === 'tomo is not gay') {
-    	message.reply('SURE JAN HAHAHAHA');
-    }
-    if (message.content === 'daisuki kyo') {
-    	message.reply('daikirai');
-    }
-    if (message.content === 'DAISUKI KYO') {
-    	message.reply('daikirai');
-    }
-    if (message.content === 'aishiteru kyo') {
-    	message.reply('daikirai');
-    }
-    if (message.content === 'AISHITERU KYO') {
-    	message.reply('daikirai');
-    }
+const responseObject = {
+  "ping": "Pong's definition means a strong an unpleasant smell.",
+  "tomo is gay": "OH HELL YEAH THIS IS TRUE",
+  "wheres kyo": "I'm right fucking here."
+  "im gay for kyo": "woah there buckaroo."
+};
+
+client.on("message", (message) => {
+  if(responseObject[message.content]) {
+    message.channel.send(responseObject[message.content]);
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY

ab20e61575d1685ecb7c19b264f7d6bcfc6f87bf
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-03-01 16:49:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('message', message => {
         message.channel.sendMessage('Ring-a-ling!!');
         }
     if (message.content.toLowerCase() === 'tomo is gay') {
-        message.channel.sendMessage('Oh yeah absolutely.');
+        message.channel.sendMessage('Oh yeah, absolutely.');
         }
     if (message.content.toLowerCase() === 'i love kyo') {
         message.channel.sendMessage("That's so sweet!");
@@ -21,11 +21,8 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  // Send the message, mentioning the member
   channel.send(`Welcome to the server, ${member}`);
 });
 

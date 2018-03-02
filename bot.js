80f1b53a12115ec4d3f0831af35c7afcd36657ed
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-03-01 19:04:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,7 @@ client.on('message', message => {
         }
     if (message.content.toLowerCase() === 'triggered') {
         message.channel.sendMessage("https://orig00.deviantart.net/3839/f/2018/060/a/0/untitled_1__3__by_kyasurln-dc4o53d.png");
+});   
 
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'member-log');

c42d997c65c69508056e7f223f184d0078324f9e
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-03-02 15:35:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,15 +21,7 @@ client.on('message', message => {
     if (message.content.toLowerCase() === 'triggered') {
         message.channel.sendMessage("https://orig00.deviantart.net/3839/f/2018/060/a/0/untitled_1__3__by_kyasurln-dc4o53d.png");
         }
-  if (command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  });
+});
 
 
 client.on('guildMemberAdd', member => {

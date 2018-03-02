4ba5d2b5b2fe3c3d7b7a4fafe9b88df988ff161f
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-03-02 15:30:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,29 @@ client.on('message', message => {
     if (message.content.toLowerCase() === 'triggered') {
         message.channel.sendMessage("https://orig00.deviantart.net/3839/f/2018/060/a/0/untitled_1__3__by_kyasurln-dc4o53d.png");
         }
-});   
+});
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  });
 
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'member-log');

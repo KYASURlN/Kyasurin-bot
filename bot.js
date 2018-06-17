35d6772f228f092083ceeb85e0ea329c89eaefcb
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-06-17 13:53:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
 client.on("ready", () => {
-    client.user.setStatus("dnd");
+    client.user.setActivity({game: {name: "with my code", type: 0}});
 });
 
 client.on('message', message => {

fad42492cc643be531b1e82897ca3b4f3338dd0d
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-07-16 16:22:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,59 +1,49 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on("ready", () => {
+    console.log("I am ready!");
+    client.user.setActivity('DONT TOUCH MY FUCKING RABBITS', { type: 'PLAYING' })
+      .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'READY'}`))
+      .catch(console.error);
+    client.user.setStatus('idle')
+      .then(console.log)
+      .catch(console.error);
+    console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`)
+  ;
 });
 
+const responseObject = {
+  "tomo is gay": "Fucking most true thing I've heard all year",
+  "i love kyo": "ok so what about it",
+  "i love kat": "can't say it to my face so you have to say it to bot me huh",
+  "bai i yai": "someone please put me out of my misery",
+  "dancing in september": "sometimes i dont put on my seatbelt while driving bc i hope to crash everytime i hear this",
+  "im gay": "didn't need to say it out loud we all already knew",
+  "im straight": "i smell a fucking lie",
+  "kyo": "aight aight i'm here what up",
+  "kat": "who r u talking to idk a kat",
+  "wheres kyo": "either depressed, sleeping, or working there no in between tbh",
+  "im tom": "r u sure u arent gay",
+  "im tomo": "tried to avoid the consequences i see",
+  "im a lesbian": "leahbian",
+  "nut": "https://www.dailydot.com/wp-content/uploads/228/6f/ab64a69d4cd848bad23ed0f9190a91eb.jpg"
+};
+
 client.on("message", (message) => {
-    if (message.content.toLowerCase() === 'ping') {
-        message.channel.sendMessage('Ring-a-ling!!');
-        } else
-    if (message.content.toLowerCase() === 'tomo is gay') {
-        message.channel.sendMessage('Can confirm tbh');
-        }
-    if (message.content.toLowerCase() === 'i love kyo') {
-        message.channel.sendMessage("nice");
-        }
-    if (message.content === 'what is my avatar') {
-        message.reply(message.author.avatarURL);
-        }
-    if (message.content.toLowerCase() === 'triggered') {
-        message.channel.sendMessage("https://file.toyhou.se/images/7336489_5xvRwS5bylHdn0y.png");
-        }
-    if (message.content.toLowerCase() === 'bai i yai') {
-        message.channel.sendMessage("Im going to fucking eat my left foot");
-        }
-    if (message.content.toLowerCase() === 'dancing in september') {
-        message.channel.sendMessage("STOPSOTPSOTPSOPTOSPOTPSOPTOPSOTPED YAMETEAM EYAMET");
-        }
-    if (message.content.toLowerCase() === 'im gay') {
-        message.channel.sendMessage("u dont need to say it we already knew");
-        }
-    if (message.content.toLowerCase() === '^ u ^') {
-        message.channel.sendMessage("if tomo is saying that pls stop ur killing me");
-        }
-    if (message.content.toLowerCase() === 'im straight') {
-        message.channel.sendMessage("this is a fucking big fat lie");
-        }
-    if (message.content.toLowerCase() === 'kyo') {
-        message.channel.sendMessage("what do you need?");
-        }
-     if (message.content.toLowerCase() === 'kat') {
-        message.channel.sendMessage("aight what up");
-        }
-     if (message.content.toLowerCase() === 'wheres kyo') {
-        message.channel.sendMessage("either depressed, sleeping, or working there no in between tbh");
-        }
-     if (message.content.toLowerCase() === 'im tom') {
-        message.channel.sendMessage("you really want to be stabbed today dont you huh");
-        }
-     if (message.content.toLowerCase() === 'nut') {
-        message.channel.sendMessage("https://www.dailydot.com/wp-content/uploads/228/6f/ab64a69d4cd848bad23ed0f9190a91eb.jpg");
-        }
-     if (message.content.toLowerCase() === 'im a lesbian') {
-        message.channel.sendMessage("leahbian");
-        }
+  if(responseObject[message.content.toLowerCase()]) {
+    message.channel.send(responseObject[message.content.toLowerCase()]);
+  }
+  if(message.author.id !== config.ownerID) return;
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("Ring-a-ling!!");
+  } 
+  if (message.content.startsWith(config.prefix + "triggered")) {
+    message.channel.send("https://file.toyhou.se/images/7336489_5xvRwS5bylHdn0y.png");
+  } 
 });
 
 // THIS  MUST  BE  THIS  WAY
